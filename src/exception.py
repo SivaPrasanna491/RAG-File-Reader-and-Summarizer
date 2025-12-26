bc62b1f1b670e1736a977be49563d0eddc601ff8
Siva Prasanna <shivachatti66@gmail.com>
2025-12-26 12:54:12 +0530

    exception and logger file added

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name[{0}] and line number [{1}] and message [{2}]".format(
+        filename, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+def Custome_Exception(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error=error_message, error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
+
+        
